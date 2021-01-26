e1a582c28818da70f527591171514735de897f85
cizamihigo <cizamihigochristian@gmail.com>
2021-01-26 12:31:14 +0200

    simple database Faculty creation in PLSQL Oracle DB

---
+++ plsqlDB.sql
@@ -0,0 +1,7 @@
+create table fuculty(
+	F_id int primary key not null,
+	F_description varchar(45) not null,
+	);
+insert into fuculty values(1,'SOCIAL SCIENCE');
+insert into fuculty values(2,'COMPUTER SCIENCE');
+insert into fuculty values(3,'IT');
